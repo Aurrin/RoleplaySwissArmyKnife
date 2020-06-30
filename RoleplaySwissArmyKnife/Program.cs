d83f81e19d6e5e991db099e18214616cb5ae23ee
Aurrin <quantumkitsune@gmail.com>
2020-06-29 21:43:38 -0500

    Clean up some comments that should have been removed before commit.

--- RoleplaySwissArmyKnife/Program.cs
+++ RoleplaySwissArmyKnife/Program.cs
@@ -19,26 +19,8 @@ namespace RoleplaySwissArmyKnife
         static void Main(string[] args) 
             => new Program().MainAsync(args).GetAwaiter().GetResult();
 
-        //private DiscordSocketClient _client  ;
-        //private CommandService      _commands;
-        //private IServiceProvider    _services;
-
         private async Task MainAsync( string[] args )
         {
-            //_client   = new DiscordSocketClient();
-            //_commands = new CommandService     ();
-            //_services = new ServiceCollection  ()
-            //    .AddSingleton(_client  )
-            //    .AddSingleton(_commands)
-            //    .BuildServiceProvider();
-
-            //string token = "NzIwMDIyNzA5MDYwMzcwNTAy.XuAzTQ.EGWkLr63TVBmdovo4ixLyH9UOII";
-            //_client.Log += _client_Log;
-            //await RegisterCommandsAsync();
-            //await _client.LoginAsync(TokenType.Bot, token);
-            //await _client.StartAsync();
-            //await Task.Delay(Timeout.Infinite);
-
             // You should dispose a service provider created using ASP.NET
             // when you are finished using it, at the end of your app's lifetime.
             // If you use another dependency injection framework, you should inspect
@@ -79,75 +61,6 @@ namespace RoleplaySwissArmyKnife
                 .AddSingleton<PictureService>        ()
                 .AddSingleton<StorageService>        ()
                 .BuildServiceProvider();
-        }
-
-        //public async Task RegisterCommandsAsync()
-        //{
-        //    _client.MessageReceived += HandleCommandAsync;
-        //    //await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-        //}
-
-        private async Task HandleCommandAsync(SocketMessage args)
-        {
-            //var message = args as SocketUserMessage;
-            //var context = new SocketCommandContext(_client, message);
-            //if (message.Author.IsBot) return;
-
-            // --- Mention a user by name ------------------------------------
-
-            //await Task.Delay(3000);
-            //await message.Channel.SendMessageAsync(String.Format(
-            //    "I'm sorry {0}, did you say something?",
-            //    message.Author.Mention));
-
-            // --- Edit message of User (FAILED) -----------------------------
-
-            //var lastChan = message.Channel;
-            //await lastChan.SendMessageAsync(
-            //    "🤖 Let me fix that for you...");
-            //// Can't do this, only author of message can edit:
-            //await message.ModifyAsync( x => x.Content = message.Content.Replace("o","0") );
-
-            // --- Delete message of User ------------------------------------
-
-            //var lastChan = message.Channel;
-            //await lastChan.SendMessageAsync(String.Format(
-            //    "I'm sorry {0}, I'm afraid I can't allow you to say `{1}`.",
-            //    context.Guild.GetUser(message.Author.Id).Nickname,
-            //    message.Content));
-            //await message.DeleteAsync();
-
-            // --- Infodump about User ---------------------------------------
-
-            //var lastChan = message.Channel;
-            //var sb = new StringBuilder();
-            //foreach (var pair in new List<(string,string)> {
-            //    ( "Username", message.Author.Username ),
-            //    ( "AvatarId", message.Author.AvatarId ),
-            //    ( "Id"      , message.Author.Id.ToString() ),
-            //    ( "Nickname", context.Guild.GetUser(message.Author.Id).Nickname )
-            //})
-            //{
-            //    sb.Append(pair.Item1);
-            //    sb.Append(": ");
-            //    sb.Append(pair.Item2);
-            //    sb.Append("\n");
-            //}
-            //await lastChan.SendMessageAsync(sb.ToString());
-
-            // --- Always React with Bot Emoji -------------------------------
-
-            //await message.AddReactionAsync(new Emoji("🤖"));
-
-            // --- Original Implementation -----------------------------------
-
-            //int argPos = 0;
-            //if (message.HasStringPrefix("!", ref argPos))
-            //{
-            //    var result = await _commands.ExecuteAsync(
-            //        context, argPos, _services);
-            //    if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
-            //}
         }
     }
 }
